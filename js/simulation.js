ee783a652549824db17da89e2149ac37b6f36236
CR27k <camilo.crack27k@gmail.com>
2020-10-09 20:05:04 -0500

    refactorizacion de funciones

--- js/simulation.js
+++ js/simulation.js
@@ -1,23 +1,30 @@
+// ****** Range Slider
+
 // Capital range slider
-
-const capital_slider = document.getElementById("capital_range");
+const capital_slider = document.getElementById("capital_range") 
 const capital_output = document.getElementById("capital");
-
 // Time range slider
 const time_slider = document.getElementById("time_range");
 const time_output = document.getElementById("time");
-
 // initial values
 let capital = capital_slider.value; 
-
 // paint range
-
 let valorIncrementoCapital = 1000; // step
 let paintValue_capital = (capital / valorIncrementoCapital); 
 let paintValue_time = 65; // initial value
 let color = '#39b54a';
 
+//table
 
+//titulo
+let title_table = document.getElementById("titulo_tabla");
+//cuerpo
+let table_body = document.getElementById('table_body');
+// % total  (rentabilidad)
+let _rentabilidad_total =  document.getElementById("rentabilidad_percentage").textContent;
+// ****endTable
+
+/** Valores iniciales */
 
 // Display the default capital_slider value
 inversion_output.innerHTML = capital_output.innerHTML = formatoSoles(capital);; 
@@ -25,15 +32,19 @@ inversion_output.innerHTML = capital_output.innerHTML = formatoSoles(capital);;
 //Display default time (moths) value
 time_output.innerHTML = formatoMeses(time_slider.value);
 
-
-
 capital_slider.style.background = `linear-gradient(to right, ${color} 0%, ${color} ${paintValue_capital}%, #fff  ${paintValue_capital}%, white 100%)`
 time_slider.style.background = `linear-gradient(to right, ${color} 0%, ${color} ${paintValue_time}%, #fff  ${paintValue_time}%, white 100%)`
 
 // exec to initial load
-document.onload = calcMonthPaid();
-generarTabla(time_slider.value);   
 
+document.onload = calcMonthPaid(time_slider.value);
+generarTabla(
+    table_body,
+    time_slider.value, //meses
+    _rentabilidad_total
+    );
+
+/** Slider Range Events */
 
 // Update the current capital_slider value (each time you drag the capital_slider handle)
 capital_slider.oninput = function() {
@@ -46,9 +57,15 @@ capital_slider.oninput = function() {
     this.style.background = `linear-gradient(to right, ${color} 0%, ${color} ${paintValue}%, #fff  ${paintValue}%, white 100%)`
 
     inversion_output.innerHTML = capital_output.innerHTML = formatoSoles(_capital);
-    inversion_output.innerHTML = capital_output.innerHTML = formatoSoles(_capital);
     
-    calcMonthPaid()
+    titulo_tabla.innerHTML = tituloTabla(time_slider.value);
+    calcMonthPaid(time_slider.value)
+    month_paid.innerHTML = formatoSoles(interesMensual(time_slider.value,_capital,rentabilidad_percentage.textContent));
+    generarTabla(
+        table_body,
+        time_slider.value, //meses
+        _rentabilidad_total
+        );
     // console.log(8);
 }
 
@@ -59,10 +76,88 @@ time_slider.oninput = function(){
     let color = '#39b54a';
     this.style.background = `linear-gradient(to right, ${color} 0%, ${color} ${paintValue}%, #fff  ${paintValue}%, white 100%)`
 
-    time_output.innerHTML =  formatoMeses(_time);
-    calcMonthPaid()
-    generarTabla(time_slider.value)
+    time_output.innerHTML = formatoMeses(_time);
+    
+    titulo_tabla.innerHTML = tituloTabla(time_slider.value);
+
+    calcMonthPaid(time_slider.value)
+    //tiempo, capital, rentabilidadTotal
+    month_paid.innerHTML = formatoSoles(interesMensual(_time,capital_slider.value,rentabilidad_percentage.textContent));
+    generarTabla(
+        table_body,
+        time_slider.value, //meses
+        _rentabilidad_total
+        )
+
 }
+
+// Calc profit - calculos de rentabilidad 
+
+function calcMonthPaid(tiempo)
+{
+    
+    const rentabilidad = document.getElementById("rentabilidad_percentage");
+    let percentage = '12 %';
+    switch (tiempo) {
+        case "6":
+            percentage='12 %'
+            break;
+        case "12":
+            percentage='28 %'
+            break;
+        case "18":
+            percentage='42 %'    
+            break;
+        case "24":
+            percentage='60 %'
+            break;
+    
+        default:
+            break;
+    }
+    // month_paid_output.innerHTML = formatoSoles(pago);
+    rentabilidad.innerHTML = percentage;
+    
+}
+
+function interesMensual(tiempo, capital, rentabilidadTotal){
+    let _porcentaje = RentabilidadMensual(rentabilidadTotal,tiempo)
+    return (_porcentaje*capital)/100;
+}
+
+
+function generarTabla(table_body,nroMeses,_rentabilidad_total){
+
+    // porcentaje
+    let percentage = RentabilidadMensual(_rentabilidad_total,nroMeses);
+
+    // pago mensual 
+    const month_paid = document.getElementById("month_paid");
+    let pago_mensual_interes = month_paid.textContent;
+
+    // tiempo de inversion
+    let fechas_pagos = FechasPago(nroMeses);
+
+    // generando la tabla
+    table_body.innerHTML="";
+    for (let i = 0; i < nroMeses; i++) {
+        let mes = i+1;
+        let fila = document.createElement("tr");
+        fila.appendChild(crearCell(mes))
+        fila.appendChild(crearCell(percentage))
+        fila.appendChild(crearCell(pago_mensual_interes))
+        fila.appendChild(crearCell(fechas_pagos[i]))
+
+        table_body.appendChild(fila);
+    }
+    // resultados 
+    return table_body;
+}  
+
+function tituloTabla(nroMeses){
+    return 'Por '+ formatoMeses(nroMeses) ;
+}
+
 // percentage to time range
 function timePercentage(time)
     {
@@ -105,75 +200,9 @@ function formatoMeses(tiempo){
     return tiempo + " meses";
 }
 
-// Calc profit - calculos de rentabilidad 
-
-function calcMonthPaid()
+function FechasPago(tiempo)
 {
-    const capital = document.getElementById("capital_range").value;
-    const time  = document.getElementById("time_range").value;
-    const month_paid = document.getElementById("month_paid");
-    const rentabilidad = document.getElementById("rentabilidad_percentage");
-    let r =0.02; //2%
-    let pago = r * capital;
-    let percentage = '12 %';
-    switch (time) {
-        case "6":
-            percentage='12 %'
-            r= 0.0200 //2.0%
-            break;
-        case "12":
-            percentage='28 %'
-            r= 0.02333 //2.3%
-            break;
-        case "18":
-            percentage='42 %'    
-            r= 0.02333 //2.3%
-            break;
-        case "24":
-            percentage='60 %'
-            r= 0.0250 //2.5%
-            break;
     
-        default:
-            break;
-    }
-    pago = r * capital;
-    month_paid.innerHTML = formatoSoles(pago);
-    rentabilidad.innerHTML = percentage;
-    
-}
-function generarTabla(nroMeses){
-    let title_table = document.getElementById("titulo_tabla");
-    let table_body = document.getElementById('table_body');
-    let _rentabilidad_total =  document.getElementById("rentabilidad_percentage").textContent;
-    let percentage = RentabilidadMensual(_rentabilidad_total,time_slider.value);
-    const month_paid = document.getElementById("month_paid")
-    let interes = month_paid.textContent;
-
-    let fecha = FechasPago();
-
-    table_body.innerHTML="";
-    for (let i = 0; i < nroMeses; i++) {
-
-        let mes = i+1;
-        
-        
-
-        let fila = document.createElement("tr");
-        
-        fila.appendChild(crearCell(mes))
-        fila.appendChild(crearCell(percentage))
-        fila.appendChild(crearCell(interes))
-        fila.appendChild(crearCell(fecha[i]))
-
-        table_body.appendChild(fila);
-    }
-    title_table.innerHTML = 'Por '+ formatoMeses(nroMeses) ;
-}  
-
-function FechasPago()
-{
-    const tiempo  = document.getElementById("time_range").value;
     let fecha_actual = new Date();
     let dia = fecha_actual.getDate();
     let mes = fecha_actual.getMonth()+1;
@@ -218,13 +247,11 @@ function FechasPago()
        
     }
     return data;
-
-    //if(dia>30)
 }
 
-function RentabilidadMensual(texto,tiempo){
-    texto =  texto.substring(0,2)
-    let porcentaje = texto/tiempo;
+function RentabilidadMensual(textoPorcentajeTotal,tiempo){
+    textoPorcentajeTotal =  textoPorcentajeTotal.substring(0,2)
+    let porcentaje = textoPorcentajeTotal/tiempo;
 
     return porcentaje.toFixed(3); // 3 decimales
 
